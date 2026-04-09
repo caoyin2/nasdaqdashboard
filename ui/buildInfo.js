cd60971410c9754a47a364a61df2e18478ef861d
caoyin2 <511912943@qq.com>
2026-04-09 11:30:03 +0800

    chore: stamp bulk quote build metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "2dd42d3",
-  fullSha: "2dd42d3d1c63efa6ba4b8c4915be4ecb00fc4fb0",
-  message: "fix: require fresh network data on first load",
-  committedAt: "2026-04-08 10:36:37",
+  shortSha: "6570805",
+  fullSha: "6570805831a15e3c24657f55bc259b182e0e52ce",
+  message: "feat: use bulk quotes for 1d star and sector panels",
+  committedAt: "2026-04-09 11:29:50",
 };
