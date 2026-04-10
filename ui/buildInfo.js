1e1fa385498fe0c99433efe1bc13db91e1e73aa4
caoyin2 <511912943@qq.com>
2026-04-10 14:43:06 +0800

    chore: stamp sa slugs migration metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "7d1cde8",
-  fullSha: "7d1cde8f0bbf4e7b6c8366dfeddd7865f99b53f6",
-  message: "fix: allow star manager delete clicks",
-  committedAt: "2026-04-10 14:08:57",
+  shortSha: "56acd69",
+  fullSha: "56acd69b98134f6ea5ba2417e8fb043cb7f5176f",
+  message: "refactor: replace runtime search calls with sa slugs",
+  committedAt: "2026-04-10 14:42:48",
 };
