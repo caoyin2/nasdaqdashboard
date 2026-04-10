c0f4f60f09fc924c84bc3c70d83daf17f3d52548
caoyin2 <511912943@qq.com>
2026-04-10 14:01:16 +0800

    chore: stamp star add validation metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "50ad0be",
-  fullSha: "50ad0be7db8be7685d4e7a9fd9610628eee77357",
-  message: "fix: pin mobile star manager to top",
-  committedAt: "2026-04-10 13:35:17",
+  shortSha: "6e1a8be",
+  fullSha: "6e1a8bea337e9749648cb92e75b906987f7324e8",
+  message: "fix: validate star additions via sa slugs",
+  committedAt: "2026-04-10 14:01:00",
 };
