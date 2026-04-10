2d3b6b64a0e84db6961110ec1fe7dbc3f55a1b5b
caoyin2 <511912943@qq.com>
2026-04-10 11:09:29 +0800

    chore: stamp sparkline latest layout metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "3e4c019",
-  fullSha: "3e4c019df5112f67cda1655d2a9a2a62b1d9ca18",
-  message: "fix: restore latest time under non-1d sparklines",
-  committedAt: "2026-04-10 10:51:14",
+  shortSha: "f8b8f60",
+  fullSha: "f8b8f60947e3ea475bb3b605a41a36f31a06cc43",
+  message: "fix: swap sparkline card latest time and delta",
+  committedAt: "2026-04-10 11:09:12",
 };
