dd943fa3da259370597adbd0516888ec5e6ad43a
caoyin2 <511912943@qq.com>
2026-04-10 15:29:06 +0800

    chore: stamp overview success rule metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "56acd69",
-  fullSha: "56acd69b98134f6ea5ba2417e8fb043cb7f5176f",
-  message: "refactor: replace runtime search calls with sa slugs",
-  committedAt: "2026-04-10 14:42:48",
+  shortSha: "5eb2b31",
+  fullSha: "5eb2b310c4a04fea5f0c8af5d123564cff8684f4",
+  message: "fix: relax overview panel success criteria",
+  committedAt: "2026-04-10 15:28:52",
 };
