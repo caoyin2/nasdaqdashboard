e220894f3396b51168c9769e13ef1eaf924f51a8
caoyin2 <511912943@qq.com>
2026-04-10 10:31:41 +0800

    chore: stamp sparkline layout metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "3cc3a39",
-  fullSha: "3cc3a39dbd6adfc3e1d961a5b2734e4332c5b6a6",
-  message: "feat: add non-1d sparklines to star and sector cards",
-  committedAt: "2026-04-10 10:12:34",
+  shortSha: "3c809d8",
+  fullSha: "3c809d8165b9716dbf7177cd77f633f02e045f77",
+  message: "fix: align sparklines beside heatmap percentages",
+  committedAt: "2026-04-10 10:31:26",
 };
