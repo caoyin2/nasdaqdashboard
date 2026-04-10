41077254001cf9d2e09576ff3cbdc201b83f3ab2
caoyin2 <511912943@qq.com>
2026-04-10 12:03:42 +0800

    chore: stamp star tech list manager metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "a3a2dc5",
-  fullSha: "a3a2dc5d4a6f4a2e412639b205d747ff8ee60508",
-  message: "feat: load star tech list from kv",
-  committedAt: "2026-04-10 11:43:05",
+  shortSha: "d3fa508",
+  fullSha: "d3fa508739d0401b4dceb8fe8baa95432711baff",
+  message: "feat: add star tech list manager",
+  committedAt: "2026-04-10 12:03:32",
 };
