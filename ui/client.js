6f536d1dabf943a9166cbb461d4c8f531f6b47b9
caoyin2 <511912943@qq.com>
2026-04-10 08:53:12 +0800

    fix: include year in card quote times

--- ui/client.js
+++ ui/client.js
@@ -133,6 +133,7 @@ export function getClientScript() {
 
     var dtfBJSeconds = new Intl.DateTimeFormat("zh-CN", {
       timeZone: "Asia/Shanghai",
+      year: "numeric",
       month: "2-digit",
       day: "2-digit",
       hour: "2-digit",
