e8803de8763dd3d099836b4f1ac47a7b77b04a3c
caoyin2 <511912943@qq.com>
2026-04-10 13:36:02 +0800

    chore: stamp mobile manager top alignment metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "1f59e6d",
-  fullSha: "1f59e6d0d060ddda785a3fb587521acf10a8051c",
-  message: "fix: validate star list additions and refine mobile manager",
-  committedAt: "2026-04-10 13:15:18",
+  shortSha: "50ad0be",
+  fullSha: "50ad0be7db8be7685d4e7a9fd9610628eee77357",
+  message: "fix: pin mobile star manager to top",
+  committedAt: "2026-04-10 13:35:17",
 };
