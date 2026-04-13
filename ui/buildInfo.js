c160935b2a6db84b828ca428b0c35ab3e72b69fa
caoyin2 <511912943@qq.com>
2026-04-13 09:34:34 +0800

    chore: stamp long-range period metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "5eb2b31",
-  fullSha: "5eb2b310c4a04fea5f0c8af5d123564cff8684f4",
-  message: "fix: relax overview panel success criteria",
-  committedAt: "2026-04-10 15:28:52",
+  shortSha: "0054234",
+  fullSha: "00542349b53d4aa01451e41282279068d835afed",
+  message: "feat: add long-range periods to market panels",
+  committedAt: "2026-04-13 09:34:21",
 };
