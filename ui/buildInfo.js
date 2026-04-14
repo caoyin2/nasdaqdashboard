495fe7960a0c63d4d7643ca4764c1e6fd8ad351a
caoyin2 <511912943@qq.com>
2026-04-14 08:39:25 +0800

    chore: stamp stale card time metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "1aac491",
-  fullSha: "1aac491a6271b57f1dfa6939746de1166e2f28b3",
-  message: "fix: preserve mobile period tab scroll",
-  committedAt: "2026-04-13 13:52:34",
+  shortSha: "9366dd3",
+  fullSha: "9366dd3c3efaf6654a9c82cfa9560dc0a5cff3fa",
+  message: "fix: flag stale card latest times",
+  committedAt: "2026-04-14 08:39:04",
 };
