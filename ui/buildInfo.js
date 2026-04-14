e4079fa26db1cc32b2463945e9a1831deb25d1f2
caoyin2 <511912943@qq.com>
2026-04-14 09:03:25 +0800

    chore: stamp chart cache bust metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "9366dd3",
-  fullSha: "9366dd3c3efaf6654a9c82cfa9560dc0a5cff3fa",
-  message: "fix: flag stale card latest times",
-  committedAt: "2026-04-14 08:39:04",
+  shortSha: "f0fae26",
+  fullSha: "f0fae2638c5a9a5a09103238ce118079982efa41",
+  message: "fix: bust SeekingAlpha chart cache",
+  committedAt: "2026-04-14 09:03:16",
 };
