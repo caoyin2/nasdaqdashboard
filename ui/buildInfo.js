f1ca484781fc52fb1886b26ef97ec652875b2bd4
caoyin2 <511912943@qq.com>
2026-04-15 08:25:29 +0800

    chore: stamp mobile index label metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "f0fae26",
-  fullSha: "f0fae2638c5a9a5a09103238ce118079982efa41",
-  message: "fix: bust SeekingAlpha chart cache",
-  committedAt: "2026-04-14 09:03:16",
+  shortSha: "5af1804",
+  fullSha: "5af180462fd6d107f460248d83590317594f83b2",
+  message: "fix: hide mobile overview index label",
+  committedAt: "2026-04-15 08:25:15",
 };
