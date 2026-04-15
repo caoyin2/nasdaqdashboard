773c4eaa4eaffa7001d7cb94e8cc79b49dcc6129
caoyin2 <511912943@qq.com>
2026-04-15 08:28:40 +0800

    chore: stamp overview label fix metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "5af1804",
-  fullSha: "5af180462fd6d107f460248d83590317594f83b2",
-  message: "fix: hide mobile overview index label",
-  committedAt: "2026-04-15 08:25:15",
+  shortSha: "7c5eb5b",
+  fullSha: "7c5eb5b138a568e99adf5f26f8bd70519a2ad763",
+  message: "fix: target mobile overview index label",
+  committedAt: "2026-04-15 08:28:14",
 };
