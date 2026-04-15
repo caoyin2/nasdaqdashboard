9142bc4f1f87bd3f86d408240404b5af320d889b
caoyin2 <511912943@qq.com>
2026-04-15 15:52:48 +0800

    chore: stamp fund premium adjustment metadata

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "c24670c",
-  fullSha: "c24670c1cbee9a3c1f1639d22d34629c485be98d",
-  message: "fix: correct fund premium cards",
-  committedAt: "2026-04-15 14:40:47",
+  shortSha: "33614df",
+  fullSha: "33614df6d2057e225d06dde1def25fb3163fffe5",
+  message: "fix: adjust fund premium display",
+  committedAt: "2026-04-15 15:52:32",
 };
