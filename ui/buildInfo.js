9b2944044dc4a60c4593f06d81aa9aa938a2fc55
caoyin2 <511912943@qq.com>
2026-04-16 10:48:20 +0800

    chore: stamp fund tab order

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "484cc96",
-  fullSha: "484cc96db79e7850171cbfbea94d9ede23ee83ba",
-  message: "fix: rename fund premium nav",
-  committedAt: "2026-04-16 10:33:25",
+  shortSha: "ec0859f",
+  fullSha: "ec0859f9927cca8be7feea371d450edc41c442ea",
+  message: "fix: reorder fund premium tab",
+  committedAt: "2026-04-16 10:48:17",
 };
