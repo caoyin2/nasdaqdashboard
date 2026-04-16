fae2af9060871bd8640978ceee59973457eb9028
caoyin2 <511912943@qq.com>
2026-04-16 09:55:50 +0800

    chore: stamp fund icon update

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "22421d8",
-  fullSha: "22421d8007faaa1766c1d3d00634411c3cd7e5d9",
-  message: "fix: align fund premium visuals",
-  committedAt: "2026-04-16 09:31:22",
+  shortSha: "07e801e",
+  fullSha: "07e801e40684841c500d1fa40dfd1cc683882832",
+  message: "fix: refine fund icon backgrounds",
+  committedAt: "2026-04-16 09:55:49",
 };
