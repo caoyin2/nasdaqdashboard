afa64f7005e04ba33f995dde9452043e1136e8fd
caoyin2 <511912943@qq.com>
2026-04-16 10:24:52 +0800

    chore: stamp fund panel rename

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "84b5fc6",
-  fullSha: "84b5fc67357d5bf0d34ce894151acb026eb93787",
-  message: "fix: use compact fund icons",
-  committedAt: "2026-04-16 10:09:11",
+  shortSha: "aa6e7de",
+  fullSha: "aa6e7de7a6a69e56c84444759891c4f7cc95ce80",
+  message: "fix: rename fund premium panel",
+  committedAt: "2026-04-16 10:24:48",
 };
