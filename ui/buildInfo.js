0a1b46ea4945ea7ba8e45ecbbc39dae96abc63a4
caoyin2 <511912943@qq.com>
2026-04-16 10:33:28 +0800

    chore: stamp fund premium nav rename

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "aa6e7de",
-  fullSha: "aa6e7de7a6a69e56c84444759891c4f7cc95ce80",
-  message: "fix: rename fund premium panel",
-  committedAt: "2026-04-16 10:24:48",
+  shortSha: "484cc96",
+  fullSha: "484cc96db79e7850171cbfbea94d9ede23ee83ba",
+  message: "fix: rename fund premium nav",
+  committedAt: "2026-04-16 10:33:25",
 };
