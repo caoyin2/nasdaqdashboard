9678da36fa01db185d9c18a91e8c38ba25add4ec
caoyin2 <511912943@qq.com>
2026-04-16 11:33:41 +0800

    chore: stamp global refresh button

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "a1c11dc",
-  fullSha: "a1c11dc27ba391845047337b403ab9191fb4d63c",
-  message: "feat: add lof premium detail view",
-  committedAt: "2026-04-16 11:22:00",
+  shortSha: "6daa425",
+  fullSha: "6daa42561a7ff3ae441c86eaf46d9dc9b0cd6a8d",
+  message: "feat: add global refresh button",
+  committedAt: "2026-04-16 11:33:33",
 };
