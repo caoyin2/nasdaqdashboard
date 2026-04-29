b04a8b47e4194be6f9b777d1b1d12572cea95887
caoyin2 <511912943@qq.com>
2026-04-29 15:42:54 +0800

    chore: stamp star pe restore

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "1f4eab8",
-  fullSha: "1f4eab866a0d04b44cfbc108b2a45f8b926441af",
-  message: "fix: fetch star forward pe only once per session",
-  committedAt: "2026-04-29 15:18:55",
+  shortSha: "ea1995b",
+  fullSha: "ea1995b1758ddb6b0989afac74815242a52f7f76",
+  message: "fix: restore star forward pe data",
+  committedAt: "2026-04-29 15:42:44",
 };
