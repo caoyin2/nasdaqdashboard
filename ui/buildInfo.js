5ba751c14478877209b04250f0f2b065c1f4ab5d
caoyin2 <511912943@qq.com>
2026-04-30 10:02:56 +0800

    Revert "chore: stamp star target alignment"
    This reverts commit abf2925065b83aea9d52c5e82d3a1ecccb740bc3.

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "27c9889",
-  fullSha: "27c9889bf943bc209e3fa64f3247d5131c6370aa",
-  message: "fix: align star target and latest time",
-  committedAt: "2026-04-30 09:52:51",
+  shortSha: "8c42549",
+  fullSha: "8c4254908618fea94e6f9fddcbbadc8ea65155be",
+  message: "feat: show stockanalysis target price",
+  committedAt: "2026-04-30 09:34:27",
 };
