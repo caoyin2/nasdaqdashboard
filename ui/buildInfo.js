89f706e5fcd22cc7fb5d373415ded8b695c58b84
caoyin2 <511912943@qq.com>
2026-05-06 16:38:33 +0800

    chore: stamp star pe loss state

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "3fca423",
-  fullSha: "3fca4231fe9a06f39a44412fe894d16fcd6e2966",
-  message: "feat: add star pe and market cap",
-  committedAt: "2026-05-06 16:20:23",
+  shortSha: "01b0dff",
+  fullSha: "01b0dff1fb19ff1ba0ba9956c49e4396c462a001",
+  message: "fix: show loss pe state on star cards",
+  committedAt: "2026-05-06 16:37:49",
 };
