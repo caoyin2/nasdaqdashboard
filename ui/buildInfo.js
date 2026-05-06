d5e24f9fd59ac58a6504147fca46dc8229daab28
caoyin2 <511912943@qq.com>
2026-05-06 16:58:05 +0800

    chore: stamp star info spacing

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "186281d",
-  fullSha: "186281d5aed2510043e894504dd2ef1f4ce47477",
-  message: "fix: normalize star target spacing",
-  committedAt: "2026-05-06 16:51:56",
+  shortSha: "c6df03a",
+  fullSha: "c6df03a8274c89558389bf22b13c6064908c25e0",
+  message: "fix: tighten star info row spacing",
+  committedAt: "2026-05-06 16:57:39",
 };
