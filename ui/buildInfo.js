cccc366261e108521dc7015d58b67847b70f551a
caoyin2 <511912943@qq.com>
2026-05-07 08:32:23 +0800

    chore: stamp target price spacing

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  shortSha: "c6df03a",
-  fullSha: "c6df03a8274c89558389bf22b13c6064908c25e0",
-  message: "fix: tighten star info row spacing",
-  committedAt: "2026-05-06 16:57:39",
+  shortSha: "fec7bc7",
+  fullSha: "fec7bc72cfff87d590b397c751530d792550da10",
+  message: "fix: remove extra target price space",
+  committedAt: "2026-05-07 08:32:03",
 };
