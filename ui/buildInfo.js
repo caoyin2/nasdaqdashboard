091e0200156eda5194da6c5d95991268632617a4
caoyin <511912943@qq.com>
2026-07-03 17:07:36 +0800

    Update displayed build info

--- ui/buildInfo.js
+++ ui/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
-  version: "8fa141e",
-  shortSha: "8fa141e",
-  fullSha: "8fa141e100f025c6549785042881209b54e6c3a3",
+  version: "f6cbd31",
+  shortSha: "f6cbd31",
+  fullSha: "f6cbd313405b61758c2d6b3205990494624b2563",
   message: "Make common weights the default view",
 };
